99b08af99e637d1cd8e47ea2745b475e1e30160e
jmcacioppo <jmcacioppo@bellsouth.net>
2017-07-20 22:14:23 -0400

    First commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var codeLibrary = angular.module('codeLibrary');
+
+// var josephcacioppo = angular.module('josephcacioppo', ['ngRoute'])
+//     .config(function($routeProvider) {
+//         $routeProvider
+//         .when('/', {
+//             templateUrl: 'templates/home.html',
+//             controller: 'HomeController'
+//         })
+//         .when('/aboutme', {
+//             templateUrl: 'templates/aboutme.html',
+//             controller: 'AboutMeController'
+//         })
+//         .when('/projects', {
+//             templateUrl: 'templates/projects.html',
+//             controller: 'ProjectsController'
+//         })
+//         .when('/resume', {
+//             templateUrl: 'templates/resume.html',
+//             controller: 'ResumeController'
+//         })
+//         .otherwise({redirectTo: '/'})
+//     });
