b271e9cd423e8fe35ebc30d062c957ea8ef6d7f7
jmcacioppo <jmcacioppo@bellsouth.net>
2017-07-26 21:44:48 -0400

    Added some usage of

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,12 +1,25 @@
 'use strict';
 //TODO: THREE WAY BINDING
 codeLibrary.controller('HomeController',
-    function($scope, $firebaseObject) {
+    function($scope, $firebaseObject, $firebaseArray) {
+        $scope.adding = {value: ''};
         rootRef.on('value', function(s) {
             console.log(s.val());
         });
 
         $scope.test = $firebaseObject(rootRef.child('HTML'));
-        console.log($scope.test);
+        $scope.test2 = $firebaseArray(rootRef);
+        // console.log($scope.test);
+        
+
+        $scope.add = () => {
+            $scope.test2.$add({"name" : $scope.adding.value})
+            .then( () => {
+                console.log('Added!');
+            })
+            .catch( (error) => {
+                console.log(error.message);
+            });
+        }
     }
 );
