7caa46e94c845c1728e30db557e213e1b52c562f
jmcacioppo <jmcacioppo0531@gmail.com>
2017-08-09 09:35:39 -0400

    Can print out names of languages with

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,40 +1,13 @@
 'use strict';
 //TODO: THREE WAY BINDING
 codeLibrary.controller('HomeController',
-    function($scope, $firebaseObject, $firebaseArray, auth) {
-        //console.log($scope.showLogin);
-        
-        $scope.topics = [];
-
+    function($scope, $firebaseObject, $firebaseArray, auth) {        
+        //Value to be added
         $scope.adding = {value: ''};
-        rootRef.on('value', (snap) => {
-            snap.forEach( (currentSnap) => {
-                $scope.topics.push(currentSnap.key);
-            });
-        });
-
-        $scope.test = $firebaseObject(rootRef.child('HTML'));
-        $scope.test2 = $firebaseArray(rootRef);
-
-        // console.log($scope.test.$id);
-        // console.log($scope.test2);
-
-        $scope.refs = [];
-
-        $scope.show = () => {            
-            $scope.topics.forEach( (value) => {
-                //$scope[count] = $firebaseObject(rootRef + '/' + value);
-                $scope.ref = firebase.database().ref('Languages/' + value);
-
-                $scope.ref.on('value', (snap) => {
-                    console.log(snap.name);
-                });
-            });
-        }
 
         //Add function
         $scope.add = () => {
-            $scope.test2.$add({"name" : $scope.adding.value})
+            $scope.arr.$add({"name" : $scope.adding.value})
             .then( () => {
                 console.log('Added!');
             })
@@ -42,5 +15,35 @@ codeLibrary.controller('HomeController',
                 console.log(error.message);
             });
         }
+        
+
+        $scope.obj = $firebaseObject(rootRef);
+        $scope.arr = $firebaseArray(rootRef);
+
+        console.log($scope.obj);
+        console.log($scope.arr);
+
+
+        //Get all keys for refs
+        $scope.keys = [];
+        rootRef.on('value', (snap) => {
+            snap.forEach( (currentSnap) => {
+                $scope.keys.push(currentSnap.key);
+            });
+        });
+
+        //Get results
+        $scope.show = () => {            
+            $scope.languages = [];
+
+            $scope.keys.forEach( (key) => {
+                //$scope[count] = $firebaseObject(rootRef + '/' + key);
+                $scope.languages.push($scope.obj[key].name);
+            });
+
+            console.log($scope.languages);
+        }
+
+        
     }
 );
